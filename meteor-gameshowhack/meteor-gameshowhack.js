36eadaa1e17447d18ff5ea969db12c1482a9d7c0
olizilla <oli@zilla.org.uk>
2013-10-26 13:54:50 +0100

    Adds home and qm routes

--- meteor-gameshowhack/meteor-gameshowhack.js
+++ meteor-gameshowhack/meteor-gameshowhack.js
@@ -1,15 +1,13 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to meteor-gameshowhack.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
+Router.map(function () {
+  this.route('home', {
+    path: '/' // match the root path
   });
+  this.route('qm')
+});
+
+
+if (Meteor.isClient) {
+
 }
 
 if (Meteor.isServer) {
